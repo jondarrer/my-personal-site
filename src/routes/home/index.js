9062bb1ba5bf26000acefba1eed43a1a6c648fac
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-07-19 11:17:11 +0100

    0 :construction: Add language switcher to homepage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,13 +3,19 @@ import { Heading } from 'theme-ui';
 import { useTranslation } from 'react-i18next';
 
 import { useLanguage } from '../../contexts';
+import { LanguageSwitcher } from '../../components';
 
 const HomePage = () => {
   const { currentLanguage } = useLanguage();
   const { t } = useTranslation();
 
   return (
-    <Heading as="h1">{t('home-page:hello', { lng: currentLanguage })}</Heading>
+    <>
+      <LanguageSwitcher />
+      <Heading as="h1">
+        {t('home-page:hello', { lng: currentLanguage })}
+      </Heading>
+    </>
   );
 };
 
