f66db95d252462ed82ffaaa2147e022a6ab6a26c
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-08-13 08:50:34 +0100

    0 :white_check_mark: Add tests for MenuLink

---
+++ src/components/menu-link/index.spec.js
@@ -0,0 +1,49 @@
+/* global describe it expect */
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+// import react-testing methods
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import MenuLink from './';
+
+describe('components/MenuLink', () => {
+  let history;
+
+  beforeEach(() => {
+    history = createMemoryHistory();
+    history.push('/the-page');
+  });
+  it("should produce a link when the page doesn't match", () => {
+    // Arrange
+    const linkContent = 'A link';
+
+    // Act
+    const res = render(
+      <Router history={history}>
+        <MenuLink to="/another-page">{linkContent}</MenuLink>
+      </Router>
+    );
+
+    // Assert
+    screen.debug();
+    expect(screen.getByText(linkContent)).toHaveAttribute('href');
+  });
+  it('should produce a when the page matches', () => {
+    // Arrange
+    const textContent = 'Plain text';
+
+    // Act
+    render(
+      <Router history={history}>
+        <MenuLink to="/the-page">{textContent}</MenuLink>
+      </Router>
+    );
+
+    // Assert
+    screen.debug();
+    expect(screen.getByText(textContent)).not.toHaveAttribute('to');
+  });
+});
