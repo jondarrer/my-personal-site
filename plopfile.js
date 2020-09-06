022d02c15da898aa6aeb01ac5a5da799d2d3dc2e
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-09-06 12:54:43 +0100

    111 :wrench: Add prompts to plopfile
    https://trello.com/c/aLTmmakh/111-adding-plop-to-automate-file-generation-within-a-project-post

--- plopfile.js
+++ plopfile.js
@@ -2,7 +2,38 @@
 module.exports = (plop) => {
   plop.setGenerator('blog-post', {
     description: 'Creating a new blog post',
-    prompts: [],
+    prompts: [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Blog post title',
+      },
+      {
+        type: 'list',
+        name: 'language',
+        message: 'Language',
+        choices: [
+          { name: 'English', value: 'en' },
+          { name: 'Romanian', value: 'ro' },
+        ],
+        default: 0,
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Description',
+      },
+      {
+        type: 'input',
+        name: 'tags',
+        message: 'Tags (comma separated list, e.g. food,soup)',
+      },
+      {
+        type: 'input',
+        name: 'pictureUrl',
+        message: 'Picture url (e.g. /images/123.jpg)',
+      },
+    ],
     actions: [],
   });
 };
