233296867a7f1ddda05d31ce00328cbdf90f7336
Jonathan Darrer <jonny@jondarrer.me.uk>
2021-03-24 16:52:22 +0000

    135 :arrow_up: Upgrade deps prior to new hydrater plugin
    https://trello.com/c/q5DUDHRT/135-integrate-with-new-release-of-react-static-site-hydrater

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ['@babel/preset-env'],
   plugins: [
-    'inline-react-svg',
+    'react-svg',
     '@babel/plugin-transform-react-jsx',
     '@babel/plugin-transform-regenerator',
     '@babel/plugin-transform-runtime',
