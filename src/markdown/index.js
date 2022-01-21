45ede3327d5c105e84401ea090f266d271d2784b
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-01-21 18:20:01 +0000

    0 📝 Begin adding Jude post

--- src/markdown/index.js
+++ src/markdown/index.js
@@ -1,3 +1,4 @@
+import jude from './jude.md';
 import stickyBbqRibsRecipe from './sticky-bbq-ribs-recipe.md';
 import learnings1519March2021 from './learnings-15-19-march-2021.md';
 import writingCypressTestsInvolvingTheBrowserSPreferColorScheme from './writing-cypress-tests-involving-the-browser-s-prefer-color-scheme.md';
@@ -23,6 +24,7 @@ import coconutFlourChocolateMuffins from './coconut-flour-chocolate-muffins.md';
 import muffinDeCiocolataCuFainaDeCocos from './muffin-de-ciocolata-cu-faina-de-cocos.md';
 
 export default {
+  'jude': jude,
   'sticky-bbq-ribs-recipe': stickyBbqRibsRecipe,
   'learnings-15-19-march-2021': learnings1519March2021,
   'writing-cypress-tests-involving-the-browser-s-prefer-color-scheme': writingCypressTestsInvolvingTheBrowserSPreferColorScheme,
---
+++ src/markdown/jude.md
@@ -0,0 +1,17 @@
+# Jude
+
+![](/images/jude.jpg Jude)
+
+```BiblePassage
+Jude, a servant of Jesus Christ and a brother of James.
+
+To those who have been called, who are loved in God the Father and kept for Jesus Christ.
+
+Mercy, peace and love be yours in abundance.
+
+Dear brothers, although I was very eager to write to you about the salvation we share, I felt compelled to write and urge you to contend for the faith once and for all entrusted to God's holy people.
+```
+
+[Jude 1-3 (NIV)](https://www.biblegateway.com/passage/?search=Jude&version=NIVUK)
+
+## Thoughts on Jude
